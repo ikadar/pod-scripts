ba3f3ae783557c01235fa78bea5b3700c0357168
Istvan Kadar <odesk.kadari@gmail.com>
2025-09-12 18:20:25 +0200

    Update pod-prince.js

--- pod-prince.js
+++ pod-prince.js
@@ -1,14 +1,17 @@
 import handleSeparators from "https://cdn.jsdelivr.net/gh/ikadar/hanging-separator@2a0c97666036c036abfa6f8bd7316efcbfb19808/separator-mono.js";
 
 
+console.log("TEST01");
 document.addEventListener("DOMContentLoaded", function () {
 
     function init() {
         const runsInPrince = (typeof Prince !== "undefined");
+        console.log("TEST02");
 
         if (runsInPrince) {
 
             Prince.Log.info("POD_PRINCE");
+            console.log("TEST03");
             
             Prince.trackBoxes = true;
 
