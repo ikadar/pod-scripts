e8200cc15c1d31d1c74190cd5ae520a328ce165d
Istvan Kadar <odesk.kadari@gmail.com>
2025-09-12 16:48:24 +0200

    Create separator-mono.js

---
+++ separator-mono.js
@@ -0,0 +1,28 @@
+const handleSeparators = () => {
+
+    console.log("HANDLING SEPARATORS 02");
+
+    const separatorNodeList = document.querySelectorAll(".separator");
+    const separators = Array.from(separatorNodeList);
+
+    separators.map((separator) => {
+        const next = separator.nextElementSibling;
+        if (next) {
+            if (getYCoordinate(separator) !== getYCoordinate(next)) {
+                separator.style.visibility = "hidden";
+            } else {
+                separator.style.visibility = "visible";
+            }
+        }
+    });
+};
+
+const getYCoordinate = (element) => {
+    if (runsInPrince) {
+        return element.getPrinceBoxes()[0].y;
+    } else {
+        return element.getBoundingClientRect().top;
+    }
+};
+
+export default handleSeparators;
